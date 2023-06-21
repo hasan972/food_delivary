a5f53d0b1b5fc6f369d467a97685d4cf153dcf63
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2023-06-21 22:58:06 +0600

    DotsIndicator

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivary/widgets/big_text.dart';
 import 'package:food_delivary/widgets/icon_and_text_widgets.dart';
 import 'package:food_delivary/widgets/small_text.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -12,7 +13,7 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
-  var _currentPageValue = 0.0;
+  double _currentPageValue = 0.0;
   double _scaleFactor = 0.8;
   double _height = 220;
   @override
@@ -22,49 +23,68 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     pageController.addListener(() {
       setState(() {
         _currentPageValue = pageController.page!;
-      // print(_currentPageValue.toString());
+        // print(_currentPageValue.toString());
       });
     });
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-     // color: Colors.amber,
-      height: 320,
-      child: PageView.builder(
-          controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context, position) {
-            return _buildPageItem(position);
-          }),
-    );
+    return Column(children: [
+      Container(
+        // color: Colors.amber,
+        height: 320,
+        child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            }),
+      ),
+      new DotsIndicator(
+        dotsCount: 5,
+        position: _currentPageValue.toInt(),
+        decorator: DotsDecorator(
+          size: const Size.square(9.0),
+          activeSize: const Size(18.0, 9.0),
+          activeShape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        ),
+      ),
+    ]);
   }
 
 //..............If i want a child container tate a size inside a parense container than using Stack......//
   Widget _buildPageItem(int index) {
     Matrix4 matrix = new Matrix4.identity();
-    if(index == _currentPageValue.floor()){
-      var currScale = 1 - (_currentPageValue-index)*(1-_scaleFactor); 
-      var currTrans = _height*(1-currScale)/2;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0,currTrans,0);
-    }else if (index == _currentPageValue.floor()+1){
-      var currScale = _scaleFactor+(_currentPageValue-index+1)*(1-_scaleFactor);
-       var currTrans = _height*(1-currScale)/2;
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0,currTrans,0);
-    }else if (index == _currentPageValue.floor()-1){
-      var currScale = 1 - (_currentPageValue-index)*(1-_scaleFactor); 
-       var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0,currTrans,0);
-    }else {
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
       var currScale = 0.8;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2,1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
     }
     return Transform(
       transform: matrix,
@@ -106,21 +126,39 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                           size: 15,
                         ),
                       )),
-                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                       SmallText(text: "4.5"),
-                      SizedBox(width:10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                       SmallText(text: "1287"),
-                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                       SmallText(text: "Comments")
-    
                     ],
                   ),
-                  SizedBox(height: 20,),
-                  Row(children: [
-                    IconAndTextWidget(icon: Icons.circle_sharp, text: "Normal", iconColor: Colors.amber),
-                     IconAndTextWidget(icon: Icons.location_on, text: "1.7km", iconColor: Colors.blue),
-                      IconAndTextWidget(icon: Icons.access_time_rounded, text: "32min", iconColor: Colors.amber)
-                  ],)
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: Colors.amber),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: Colors.blue),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconColor: Colors.amber)
+                    ],
+                  )
                 ],
               ),
             ),
